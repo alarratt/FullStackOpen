0611dd9257445014d7334f2fd1940cb4dd6d3ff3
alarratt <anthonylarratt@gmail.com>
2022-08-23 09:39:04 -0400

    Fixed first edits

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import reactDOM from 'react-dom/client';
+
+import App from './App';
+
+reactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
