c5f9a4bb11a209759ff0d5ada1be377584d30467
alarratt <anthonylarratt@gmail.com>
2022-08-26 16:07:21 -0400

    Completed the unicafe work

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,36 +5,70 @@ const Button = ({clickHandler, text}) => {
     <button onClick={clickHandler}>{text}</button>
   )
   }
-const Display = ({text, value}) => (<p>{text}: {value}</p>)
+
+const avg = (good, neutral, bad) => {
+  if (good+bad+neutral === 0){
+    return "No Ratings Yet"
+  }
+  else {
+    return(
+      ((good)-(bad))/(good+bad+neutral)
+      )
+  }
+}
+
+const percPos = (good, neutral, bad) => {
+  if (good+neutral+bad===0){
+    return ("No Ratings Yet")
+  }
+    else {
+      return (good/(good+bad+neutral))
+  }
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  if (good+bad+neutral===0){
+    return(<div><p>No Feedback Given</p></div>)
+  }
+  else {
+    return(
+      <table>
+        <tbody>
+
+          <StatisticsLine text= 'Good' value = {good} />
+          <StatisticsLine text= 'Neutral' value = {neutral} />
+          <StatisticsLine text = 'Bad' value = {bad} />
+          <StatisticsLine text ='All' value = {good+bad+neutral} />
+          <StatisticsLine text ='Average' value = {Math.round(100*avg(good, neutral, bad))/100} />
+          <StatisticsLine text ='Positive' value = {Math.round(10000*percPos(good,bad,neutral)/100)+"%"} />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+const StatisticsLine = ({text,value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+} 
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const goodFeedback = () => setGood(good+1)
-  const neutralFeedback = () => setNeutral(neutral+1)
-  const badFeedback = () => setBad(bad+1)
-  const avg = ({good, neutral, bad}) => {
-    console.log(good, neutral, bad)
-    return(
-      ((good)+(bad))/(good+bad+neutral)
-      )
-    }
   
-  console.log(good,bad,neutral)
-  console.log(avg(good,bad,neutral))
   return (
     <div>
       <h1>Give Us Feedback</h1>
-      <Button clickHandler={goodFeedback} text = "Good" />
-      <Button clickHandler={neutralFeedback} text = "Neutral" />
-      <Button clickHandler={badFeedback} text = "Bad" />
-      <Display text= 'Good' value = {good} />
-      <Display text= 'Neutral' value = {neutral} />
-      <Display text= 'Bad' value = {bad} />
-      <Display text ='All' value = {good+bad+neutral} />
-      <Display text = 'Average' value = {avg(1,1,15)} />
-      <p>{avg(1,1,15)}</p>
+      <Button clickHandler={()=>setGood(good+1)} text = "Good" />
+      <Button clickHandler={()=> setNeutral(neutral+1)} text = "Neutral" />
+      <Button clickHandler={()=> setBad(bad+1)} text = "Bad" />
+      <h1>Statistics</h1>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
  }
