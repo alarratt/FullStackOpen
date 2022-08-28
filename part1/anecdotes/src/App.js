bf66017ad7deb8fcd57659908f37980481794568
alarratt <anthonylarratt@gmail.com>
2022-08-28 09:03:24 -0400

    Part 1 Complete

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,46 @@
+
+import { useState } from 'react'
+const votes = [0,0,0,0,0,0,0]
+const Text = ({anecdote, vote}) => {
+  return(
+    <div>
+      <p>{anecdote}</p>
+      <p>has {vote} votes</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of development time ...',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState([])
+  const generateRand = () => {
+    return(
+    Math.floor(Math.random()*7)
+    )
+  }
+  const voteFor = (selected) => {
+    console.log (votes)
+    return(
+    votes[selected] = votes[selected]+1
+    )
+  }
+  return(
+    <div>
+      <Text anecdote = {anecdotes[selected]} vote = {vote} />
+      <div><button onClick = {()=>setSelected(generateRand)}>Next anecdote </button></div>
+      <div><button onClick = {()=>setVote(voteFor(selected))}>Vote</button></div>
+      <h1>Anecdote with the most votes</h1>
+      <p>{anecdotes[votes.indexOf(Math.max(...votes))]}</p>
+    </div>
+  )
+}
+export default App
