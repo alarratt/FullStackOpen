df2460bf38901cd8bb779424a34ba6039e05708a
alarratt <anthonylarratt@gmail.com>
2022-10-16 10:54:49 -0400

    Part 2b complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,41 @@
 import {useState} from 'react'
+import Filter from './Components/Filter'
+import AddPerson from './Components/AddPerson'
+import PhoneBook from './Components/PhoneBook'
 
 const App = () =>{
-  const [personsm setPersons] = useState([
-    {name: 'Arto Hellas'}
-  ])
+  const [persons, setPersons] = useState([{name: 'Arto Hellas', number: "040-123456"}])
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [filter, setFilter] = useState('')
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNum
+    }
+    if(persons.findIndex(x => x.name === newName)===-1){
+      setPersons(persons.concat(personObject))
+    }
+    else{alert(`${newName} is already in the records.`)}
+  }
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumChange = (event) => {
+    setNewNum(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          Name: <input />
-        </div>
-        <div>
-          <button type = "submit">add</button>
-        </div>
-      </form>
+      <Filter filterCO = {filter} filterByNameCO = {handleFilterChange}/>
+      <AddPerson addPerson = {addPerson} newName = {newName} handleNameChange = {handleNameChange} newNum = {newNum} handleNumChange = {handleNumChange} />
       <h2>Numbers</h2>
+      <PhoneBook persons={persons} filter = {filter}/>
     </div>
   )
 }
-
 export default App
